5d30be81dd9fcdc436e4af50eaa5d41e3bd941c2
Tom Bennett <tbennett@homeaway.com>
2015-01-07 12:34:05 +0000

    no message

--- nest.php
+++ nest.php
@@ -13,7 +13,7 @@ $nest = new Nest($username, $password);
 
 echo "Device information:\n";
 $infos = $nest->getDeviceInfo();
-//jlog($infos);
+jlog($infos);
 echo "----------\n\n";
 
 echo "Current temperature:\n";
@@ -27,3 +27,87 @@ echo $apiKey;
 $update = new Leftronic($apiKey);
 
 $update->pushNumber("myNumberStream", 123456);
+
+
+
+
+
+
+/* Helper functions */
+function json_format($json) { 
+    $tab = "  "; 
+    $new_json = ""; 
+    $indent_level = 0; 
+    $in_string = false; 
+    $json_obj = json_decode($json); 
+    if($json_obj === false) 
+        return false; 
+    $json = json_encode($json_obj); 
+    $len = strlen($json); 
+    for($c = 0; $c < $len; $c++) 
+    { 
+        $char = $json[$c]; 
+        switch($char) 
+        { 
+            case '{': 
+            case '[': 
+                if(!$in_string) 
+                { 
+                    $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1); 
+                    $indent_level++; 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case '}': 
+            case ']': 
+                if(!$in_string) 
+                { 
+                    $indent_level--; 
+                    $new_json .= "\n" . str_repeat($tab, $indent_level) . $char; 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case ',': 
+                if(!$in_string) 
+                { 
+                    $new_json .= ",\n" . str_repeat($tab, $indent_level); 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case ':': 
+                if(!$in_string) 
+                { 
+                    $new_json .= ": "; 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case '"': 
+                if($c > 0 && $json[$c-1] != '\\') 
+                { 
+                    $in_string = !$in_string; 
+                } 
+            default: 
+                $new_json .= $char; 
+                break;                    
+        } 
+    } 
+    return $new_json; 
+}
+function jlog($json) {
+    if (!is_string($json)) {
+        $json = json_encode($json);
+    }
+    echo json_format($json) . "\n";
+}
