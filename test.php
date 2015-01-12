61614c41aa01d919166d719c158063304cc1776f
Tom Bennett <tbennett@homeaway.com>
2015-01-12 09:12:31 +0000

    no message

--- test.php
+++ test.php
@@ -54,6 +54,8 @@ $update->pushNumber("temp_outside", $temp_outside);
 $update->pushNumber("temp_current_value", $temp_current);
 $update->pushNumber("temp_target_value", $temp_target);
 $update->pushNumber("temp_outside_value", $temp_outside);
+$update->pushNumber("humidity_inside", $humidity_current);
+$update->pushNumber("humidity_outside", $humidity_current);
 $update->pushImage("current_observation_image", $current_observation_image);
 
 if($heat=="true") {
